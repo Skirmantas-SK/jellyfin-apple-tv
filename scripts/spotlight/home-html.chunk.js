50a17a9551be6edf030f77e37d4f440778939ead
Skirmantas <Skirmantas.work@gmail.com>
2026-06-03 03:00:22 +0300

    Update home-html.chunk.js

--- scripts/spotlight/home-html.chunk.js
+++ scripts/spotlight/home-html.chunk.js
@@ -10,26 +10,26 @@
   <style>
     .featurediframe {
       width: 100%; display: block; border: 0; margin: 0; padding: 0;
-      height: 82vh; min-height: 640px; max-height: 900px;
+      height: 88vh; min-height: 700px; max-height: 980px;
       margin-top: 0;
     }
     @media (min-width: 1400px) {
-      .featurediframe { height: 84vh; max-height: 960px; }
+      .featurediframe { height: 90vh; max-height: 1040px; }
     }
     @media (min-width: 1920px) {
-      .featurediframe { height: 86vh; max-height: 1040px; }
+      .featurediframe { height: 92vh; max-height: 1120px; }
     }
     @media (max-width: 1024px) and (orientation: portrait) {
-      .featurediframe { height: 82vh; min-height: 560px; max-height: 840px; margin-top: 0; }
+      .featurediframe { height: 88vh; min-height: 600px; max-height: 900px; margin-top: 0; }
     }
     @media (max-width: 1024px) and (orientation: landscape) {
-      .featurediframe { height: 92vh; min-height: 380px; max-height: 620px; margin-top: 0; }
+      .featurediframe { height: 96vh; min-height: 400px; max-height: 680px; margin-top: 0; }
     }
     @media (max-width: 600px) and (orientation: portrait) {
-      .featurediframe { height: 80vh; min-height: 500px; max-height: 800px; margin-top: 0; }
+      .featurediframe { height: 86vh; min-height: 540px; max-height: 860px; margin-top: 0; }
     }
     @media (max-width: 900px) and (orientation: landscape) and (max-height: 500px) {
-      .featurediframe { height: 92vh; min-height: 340px; max-height: 560px; margin-top: 0; }
+      .featurediframe { height: 96vh; min-height: 360px; max-height: 600px; margin-top: 0; }
     }
   </style>
 
